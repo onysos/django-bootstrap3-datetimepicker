51c6cc0d1d650f5429b221d087b9028c6e3cc818
darius BERNARD <darius.bernard@onysos.fr>
2015-02-23 16:07:57 +0100

    bump version to 3.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
                                           'static/bootstrap3_datetime/js/*.js',
                                           'static/bootstrap3_datetime/js/locales/*.js', ]},
     include_package_data=True,
-    version='3.0',
+    version='3.0.1',
     description='Bootstrap3 compatible datetimepicker for Django projects.',
     long_description=open('README.rst').read(),
     author='Nakahara Kunihiko',
